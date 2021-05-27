b4e7d2f3c75c3873f795dc2d4b552043a672adad
rauzola <raul_sigoli@hotmail.com>
2021-05-27 16:57:16 -0300

    Separador, Heading e o Slider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { AppProps } from 'next/app';
-import { theme } from '../styles/theme';
+import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme'
+// import NextNProgress from 'nextjs-progressbar';
+
+// Import Swiper styles
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+import 'swiper/components/scrollbar/scrollbar.min.css';
+
+import '../styles/slider.scss';
 
 function App({ Component, pageProps }: AppProps) {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import Banner from "../components/Banner";
 import Caracteristicas from "../components/Caracteristicas";
 import { Header } from "../components/Header";
+import Separador from "../components/Separador";
+import Slider from "../components/Slider";
 
 
 export default function Home() {
@@ -11,6 +13,20 @@ export default function Home() {
       <Banner />
 
       <Caracteristicas />
+
+      <Separador />
+
+      <Heading
+        textAlign="center"
+        fontWeight="500"
+        mb={["5", "14"]}
+        fontSize={["lg", "3xl", "4xl"]}
+      >
+        Vamos nessa <br /> Então escolha seu continente
+      </Heading>
+
+      <Slider />
+      
     </Flex>
   )
 }
